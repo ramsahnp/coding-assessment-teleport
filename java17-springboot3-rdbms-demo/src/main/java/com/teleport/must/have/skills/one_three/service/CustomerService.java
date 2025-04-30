e05e800d5028b9fa763da1479038318230828343
ramsahnp <ramsahnp@gmail.com>
2025-04-30 09:07:09 +0530

    kafka-4.Event-Driven

--- java17-springboot3-rdbms-demo/src/main/java/com/teleport/must/have/skills/one_three/service/CustomerService.java
+++ java17-springboot3-rdbms-demo/src/main/java/com/teleport/must/have/skills/one_three/service/CustomerService.java
@@ -2,9 +2,13 @@ package com.teleport.must.have.skills.one_three.service;
 
 import com.teleport.must.have.skills.one_three.entity.Customer;
 import com.teleport.must.have.skills.one_three.entity.Order;
+import com.teleport.must.have.skills.one_three.kafka.UserEventProducer;
 import com.teleport.must.have.skills.one_three.repository.CustomerRepository;
 import com.teleport.must.have.skills.one_three.repository.OrderRepository;
+import com.teleport.must.have.skills.one_three.util.KafkaUtils;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,32 +17,46 @@ import java.util.Optional;
 
 @Service
 public class CustomerService {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepo;
     private final OrderRepository orderRepo;
 
+    private final UserEventProducer userEventProducer;
+
     @Autowired
-    public CustomerService(CustomerRepository customerRepo, OrderRepository orderRepo) {
+    public CustomerService(CustomerRepository customerRepo, OrderRepository orderRepo, UserEventProducer userEventProducer) {
         this.customerRepo = customerRepo;
         this.orderRepo = orderRepo;
+        this.userEventProducer = userEventProducer;
     }
 
     @Transactional
     public void placeOrder(String customerName, String productName) {
-        Optional<Customer> customerFromDb = customerRepo.findByName(customerName);
+        try {
+            Optional<Customer> customerFromDb = customerRepo.findByName(customerName);
 
-        Customer customer;
-        if (customerFromDb.isPresent()) {
-            customer = customerFromDb.get();
-        } else {
-            customer = new Customer();
-            customer.setName(customerName);
-            customer = customerRepo.save(customer);  // Save only if customer doesn't exist
+            Customer customer;
+            if (customerFromDb.isPresent()) {
+                customer = customerFromDb.get();
+                logger.info("Old Customer, customer name: {}", customer.getName());
+            } else {
+                customer = new Customer();
+                customer.setName(customerName);
+                customer = customerRepo.save(customer);  // Save only if customer doesn't exist
+                logger.info("New customer, customer name: {}", customer.getName());
+                //userEvent.setAction("new customer added, customer id: " + customer.getId());
+            }
+            Order order = new Order();
+            order.setProduct(productName);
+            order.setCustomer(customer);
+            order = orderRepo.save(order);
+            //userEvent.setAction("new oder added, order id: " + order.getId());
+            userEventProducer.sendMessage("Hello...Consumers");
+            logger.info("Order Placed Successfully..!");
+        } catch (Exception ex) {
+            logger.info("Error while placing order error-msg: {} ", ex.getMessage());
+            ex.printStackTrace();
         }
-
-        Order order = new Order();
-        order.setProduct(productName);
-        order.setCustomer(customer);
-        orderRepo.save(order);
     }
 
 
@@ -46,11 +64,11 @@ public class CustomerService {
         return customerRepo.findCustomersWithOrders(name);
     }
 
-    public List<Order> getAllOrder(){
+    public List<Order> getAllOrder() {
         return orderRepo.findAll();
     }
 
-    public List<Customer> getAllCustomer(){
+    public List<Customer> getAllCustomer() {
         return customerRepo.findAll();
     }
 
---
+++ java17-springboot3-rdbms-demo/src/main/java/com/teleport/must/have/skills/one_three/util/KafkaUtils.java
@@ -0,0 +1,6 @@
+package com.teleport.must.have.skills.one_three.util;
+
+public class KafkaUtils {
+
+    public static final String TOPIC = "orders-topic";
+}
